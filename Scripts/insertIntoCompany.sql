e509f79ea48a2be79c4591698176340f1e558d4b
Saad Jeelani <saad.jeelani88@gmail.com>
2022-05-08 21:51:31 -0700

    added scripts for submission

---
+++ Scripts/insertIntoCompany.sql
@@ -0,0 +1,35 @@
+-- Populating company table for TECHNOLOGY FIELD use case
+INSERT INTO COMPANY(COMPANY_NAME, COMPANY_COUNTRY, COMPANY_STATE, COMPANY_STANDING, COMPANY_FIELD)
+        VALUES('Apple', 'United States', 'California', 'Corporate', 'Technology');
+INSERT INTO COMPANY(COMPANY_NAME, COMPANY_STANDING, COMPANY_COUNTRY, COMPANY_STATE, COMPANY_FIELD)
+        VALUES('Amazon', 'Corporate', 'United States', 'Washington State', 'Technology');
+INSERT INTO COMPANY(COMPANY_NAME, COMPANY_STANDING, COMPANY_COUNTRY, COMPANY_STATE, COMPANY_FIELD)
+        VALUES('Meta', 'Corporate', 'United States', 'California', 'Technology');
+INSERT INTO COMPANY(COMPANY_NAME, COMPANY_STANDING, COMPANY_COUNTRY, COMPANY_STATE, COMPANY_FIELD)
+        VALUES('Netflix', 'Corporate', 'United States', 'California', 'Technology');
+INSERT INTO COMPANY(COMPANY_NAME, COMPANY_STANDING, COMPANY_COUNTRY, COMPANY_STATE, COMPANY_FIELD)
+        VALUES('Google', 'Corporate', 'United States', 'California', 'Technology');
+INSERT INTO COMPANY(COMPANY_NAME, COMPANY_STANDING, COMPANY_COUNTRY, COMPANY_STATE, COMPANY_FIELD)
+        VALUES('IBM', 'Corporate', 'United States', 'Georgia', 'Technology');
+-- Populating company table for HOSPITALITY FIELD use case
+INSERT INTO COMPANY(COMPANY_NAME, COMPANY_COUNTRY, COMPANY_STATE, COMPANY_FIELD)
+        VALUES('ABC Pizzas', 'Canada', 'Alberta', 'Hospitality');
+INSERT INTO COMPANY(COMPANY_NAME, COMPANY_COUNTRY, COMPANY_STATE, COMPANY_FIELD)
+        VALUES('Best Burgers', 'Canada', 'British Columbia', 'Hospitality');
+INSERT INTO COMPANY(COMPANY_NAME, COMPANY_COUNTRY, COMPANY_STATE, COMPANY_FIELD)
+        VALUES('Wicked Waffles', 'United States', 'Florida', 'Hospitality');
+INSERT INTO COMPANY(COMPANY_NAME, COMPANY_COUNTRY, COMPANY_STATE, COMPANY_FIELD)
+        VALUES('Sizzling Steak', 'United States', 'Alabama', 'Hospitality');
+INSERT INTO COMPANY(COMPANY_NAME, COMPANY_COUNTRY, COMPANY_STATE, COMPANY_FIELD)
+        VALUES('Nice Nachos', 'United States', 'Alabama', 'Hospitality');
+INSERT INTO COMPANY(COMPANY_NAME, COMPANY_COUNTRY, COMPANY_STATE, COMPANY_FIELD)
+        VALUES('Krispy Krabs', 'United States', 'Alabama', 'Hospitality');
+-- Populating company table for MEDICINE FIELD use case
+INSERT INTO COMPANY(COMPANY_NAME, COMPANY_STANDING, COMPANY_COUNTRY, COMPANY_STATE, COMPANY_FIELD)
+        VALUES('Washington Hospital', 'Government', 'United States', 'California', 'Medicine');
+INSERT INTO COMPANY(COMPANY_NAME, COMPANY_STANDING, COMPANY_COUNTRY, COMPANY_STATE, COMPANY_FIELD)
+        VALUES('Fixerupper Hospital', 'Private', 'United States', 'New York', 'Medicine');
+INSERT INTO COMPANY(COMPANY_NAME, COMPANY_STANDING, COMPANY_COUNTRY, COMPANY_STATE, COMPANY_FIELD)
+        VALUES('Mayo Clinic', 'Private', 'United States', 'Washington DC', 'Medicine');
+INSERT INTO COMPANY(COMPANY_NAME, COMPANY_STANDING, COMPANY_COUNTRY, COMPANY_STATE, COMPANY_FIELD)
+        VALUES('Sunshine Hospital', 'Government', 'United States', 'Hawaii', 'Medicine');
